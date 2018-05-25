394fc71963cfe582cc5ab05075c6cbfb3247f44b
Pablo Villagran Torres <pablo.villagran@liztex.com>
2018-05-25 12:19:12 -0600

    conectado a postgresql

--- Rest/index.js
+++ Rest/index.js
@@ -2,19 +2,40 @@
 
 const express = require('express')
 const bodyparser = require('body-parser')
+const { Pool, Client } = require('pg')
+
 const app = express()
 const port = process.env.PORT || 3000
+const conn = {	  user: 'liztex',
+				  host: '172.16.23.153',
+				  database: 'dbliztex1',
+				  password: 'golosin',
+				  port: 5432,
+			}
+
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
 app.get('/api/product',(req, res) =>{
-
-	res.send( 200 , {products: [] })
+		let products = [];
+		const pool = new Pool(conn)
+		pool.query('SELECT * FROM lzty_variable LIMIT 1;', (err, resp) => {
+			console.log(resp)
+		  	for (var i = resp.rows.length - 1; i >= 0; i--) {
+			  	products[i] = ({ id : resp.rows[i].lzty_variable, desc: resp.rows[i].descrip, abr: resp.rows[i].abr })
+			  }
+			  pool.end()
+		  	res .status(200) 
+				.send( {product: products })
+		})
+		
+		
 })
 
 app.get('/api/product/:productId',(req, res) =>{
-
-	res.send(200 , {products: [] })	
+	console.log('ok')
+	res .status(200)
+		.send( {products: [] })	
 })
 
 app.post('/api/product',(req, res) =>{
@@ -23,15 +44,22 @@ app.post('/api/product',(req, res) =>{
 		.send({ message: 'producto creado' })
 })
 
-app.get('/api/product/:productId',(req, res) =>{
 
-	res.send({ status: 200 })
+
+
+/*
+app.put('/api/product/:productId',(req, res) =>{
+
+	res .status(200)
+		.send({ message: 'producto creado' })
 })
 
 app.get('/api/product/:productId',(req, res) =>{
 
-	res.send({ status: 200 })
+	res .status(200)
+		.send({ message: 'producto creado' })
 })
+*/
 
 app.listen(port,() => {
 	console.log(`API REST en localhost:${port}`)
