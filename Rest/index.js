809c9e2bb2175b77ccb2fa56151326145a834f0c
Pablo Villagran Torres <pablo.villagran@liztex.com>
2018-06-05 09:12:13 -0600

    se agrega la libreria moment y jwt

--- Rest/index.js
+++ Rest/index.js
@@ -2,6 +2,8 @@
 
 const express = require('express')
 const bodyparser = require('body-parser')
+const jwt = require('jwt-simple')
+const moment = require('moment')
 const { Pool, Client } = require('pg')
 
 const app = express()
@@ -11,37 +13,114 @@ const conn = {	  user: 'liztex',
 				  database: 'dbliztex1',
 				  password: 'golosin',
 				  port: 5432,
+				  client_encoding : 'utf8',
 			}
 
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
+
+app.get('/api/token',(req, res) =>{
+		
+	const payload = {
+		sub: "91509",
+		iat: moment().unix() ,
+		exp: moment().add(1 , 'hour' ).unix() ,
+	}
+
+  	res .status(200) 
+		.send( {message: 'success',
+				token: jwt.encode(payload, 'clavetoken')})
+})
+
+
 app.get('/api/product',(req, res) =>{
 		let products = [];
 		const pool = new Pool(conn)
-		pool.query('SELECT * FROM lzty_variable LIMIT 1;', (err, resp) => {
-			console.log(resp)
-		  	for (var i = resp.rows.length - 1; i >= 0; i--) {
-			  	products[i] = ({ id : resp.rows[i].lzty_variable, desc: resp.rows[i].descrip, abr: resp.rows[i].abr })
-			  }
-			  pool.end()
-		  	res .status(200) 
-				.send( {product: products })
-		})
-		
-		
+			pool.query('SET CLIENT_ENCODING TO "SQL_ASCII"; SELECT lzty_variable, descrip, abr FROM lzty_variable ;', (err, resp) => {
+			if (resp[1].rowCount < 1)
+			{
+				res .status(404) 
+					.send( {message: 'Ha ocurrido algun error. verifique!',
+							product: products })	
+			}
+			else
+			{	
+				for (var i = resp[1].rows.length - 1; i >= 0; i--) {
+				  	products[i] = ({ id : resp[1].rows[i].lzty_variable, 
+				  					desc: resp[1].rows[i].descrip, 
+				  					abr : resp[1].rows[i].abr })
+				  }
+				pool.end()
+			  	res .status(200) 
+					.send( {message: 'success',
+							product: products })
+			}
+		})	
 })
 
 app.get('/api/product/:productId',(req, res) =>{
-	console.log('ok')
-	res .status(200)
-		.send( {products: [] })	
+	let products = [];
+	const pool = new Pool(conn)
+	if( isNaN(req.params.productId) ){
+		res .status(400) 
+			.send( {message: 'Ha ocurrido algun error con el parametro. verifique!',
+					product: products })	
+	}
+	else{
+		pool.query(`SET CLIENT_ENCODING TO "SQL_ASCII"; SELECT lzty_variable, descrip, abr FROM lzty_variable  WHERE lzty_variable = ${req.params.productId};`, (err, resp) => {
+			if (resp[1].rowCount < 1)
+			{
+				res .status(404) 
+					.send( {message: 'Ha ocurrido algun error. verifique!',
+							product: products })	
+			}
+			else
+			{	
+				for (var i = resp[1].rows.length - 1; i >= 0; i--) {
+				  	products[i] = ({ id : resp[1].rows[i].lzty_variable, 
+				  					desc: resp[1].rows[i].descrip, 
+				  					abr : resp[1].rows[i].abr })
+				  }
+				pool.end()
+			  	res .status(200) 
+					.send( {message: 'success',
+							product: products })
+			}
+		})
+	}
 })
 
 app.post('/api/product',(req, res) =>{
-	console.log(req.body)
-	res .status(200)
-		.send({ message: 'producto creado' })
+	let products = [];
+	const pool = new Pool(conn)
+	if( isNaN(req.body.padre) ){
+		res .status(400) 
+			.send( {message: 'Ha ocurrido algun error con el parametro. verifique!',
+					product: products })	
+	}
+	else{
+		pool.query(`SET CLIENT_ENCODING TO "SQL_ASCII"; SELECT prf_guardavariable( '${req.body.descrip}' , '${req.body.abr}' , ${req.body.padre} , ${req.body.usuario});`, (err, resp) => {
+			if (resp[1].rowCount < 1)
+			{
+				res .status(404) 
+					.send( {message: 'Ha ocurrido algun error. verifique!',
+							product: products })	
+			}
+			else
+			{	
+				for (var i = resp[1].rows.length - 1; i >= 0; i--) {
+				  	products[i] = ({ id : resp[1].rows[i].lzty_variable, 
+				  					desc: resp[1].rows[i].descrip, 
+				  					abr : resp[1].rows[i].abr })
+				  }
+				pool.end()
+			  	res .status(200) 
+					.send( {message: 'success',
+							product: products })
+			}
+		})
+	}
 })
 
 
